34ef22319061d6c731841f3ddc2901f589334ede
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-19 23:46:50 +0300

    4. More Components (Extending Post Page)

--- alx-project-0x01/components/common/PostModal.tsx
+++ alx-project-0x01/components/common/PostModal.tsx
@@ -1,4 +1,4 @@
-import {PostData, PostModalProps} from "@/interfaces";
+import { PostData, PostModalProps } from "@/interfaces";
 import React, {useState} from "react";
 
 const PostModal: React.FC<PostModalProps> = ({onClose, onSubmit}) => {
