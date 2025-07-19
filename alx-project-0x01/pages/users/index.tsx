37446e3393fcb1e2c72b44878570b88f91f77f4e
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-20 00:37:44 +0300

    5. Implement the User Modal

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,19 +4,25 @@
 // index page for Users related pages
 import React from 'react';
 import Header from "@/components/layout/Header";
-import { UserProps } from "@/interfaces";
+import { UserProps, UserData } from "@/interfaces";
 import UserCard from "@/components/common/UserCard";
+import { useState } from "react";
+import UserModal from '@/components/common/UserModal';
 
 
 const Users: React.FC<UserProps[]> = ({ posts }) => {
-    console.log(posts);
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [user, setUser] = useState<UserData | null>(null);
+    const handleAddPost = (newUser: UserData) => {
+        setUser({ ...newUser, id: posts.length + 1 }); // Simulating ID assignment 
+    };
     return (
         <div className="flex flex-col h-screen">
             <Header />
             <main className="p-4">
                 <div className="flex justify-between">
                     <h1 className=" text-2xl font-semibold">User List</h1>
-                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
+                    <button onClick={()=>setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">Add User</button>
                 </div>
                 <div className="grid grid-cols-3 gap-2 ">
                     {
@@ -26,19 +32,22 @@ const Users: React.FC<UserProps[]> = ({ posts }) => {
                     }
                 </div>
             </main>
+            {isModalOpen && (
+                <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+            )}
         </div>
     );
 }
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const posts = await response.json()
 
-  return {
-    props: {
-      posts
+    return {
+        props: {
+            posts
+        }
     }
-  }
 }
 
 export default Users;
