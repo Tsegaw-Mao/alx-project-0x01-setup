bf3aa5e23c92685a94b30be4645fe34e4c1c70b8
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-07-19 22:34:52 +0300

    3. Implement Users Card Components

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,19 +1,46 @@
 // pages/posts/index.tsx
 
 
-// index page for Posts related pages
-import React from 'react';
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
+let counter = 0;
 
 
-const Post: React.FC = () => {
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+    console.log(posts)
+    counter += 1;
+    console.log("Counters:", counter);
     return (
         <div className="flex flex-col h-screen">
             <Header />
-            <div className="flex justify-center items-center h-screen">
-                <h1 className="text-7xl font-thin">Posts Page</h1>
-            </div>
+            <main className="p-4">
+                <div className="flex justify-between">
+                    <h1 className=" text-2xl font-semibold">Post Content</h1>
+                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+                </div>
+                <div className="grid grid-cols-3 gap-2 ">
+                    {
+                        posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+                            <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+                        ))
+                    }
+                </div>
+            </main>
         </div>
-    );
+    )
 }
-export default Post;
+
+
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const posts = await response.json()
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
+
+export default Posts;
